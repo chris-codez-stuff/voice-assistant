fd076c95621cade8d71f8cce0407cb64003a877d
chris-codez-stuff <chrisbinay.b@gmail.com>
2024-07-30 22:13:06 +0530

    Implement talk back feature and fix bugs

--- talk_back.py
+++ talk_back.py
@@ -10,21 +10,24 @@ def text_to_audio(text):
     tts = gTTS(text=text, lang=language, slow=False)
     tts.save("output.mp3")
 
-    audio = AudioSegment.from_mp3("output.mp3")
-    audio.export("output.wav", format="wav")
-
 
 def play():
-    pygame.mixer.init()
-    pygame.mixer.music.load("output.mp3")
-    pygame.mixer.music.play()
+    try:
+        pygame.mixer.quit()  # Reset state
+        pygame.mixer.init()
+        print(f"Loading output.mp3")
+        pygame.mixer.music.load("output.mp3")
+        pygame.mixer.music.play()
 
-    while pygame.mixer.music.get_busy():
-        time.sleep(1)
+        while pygame.mixer.music.get_busy():
+            time.sleep(1)
 
-    print("Conversion completed and audio played successfully.")
+        print("Audio played successfully.")
+
+    except Exception as e:
+        print(f"Error in play: {e}")
 
 
 if __name__ == "__main__":
-    text_to_audio("Talking computer")
+    text_to_audio("Talking computer is very cool")
     play()
